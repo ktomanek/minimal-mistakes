ee79ae04f314ab17624e83f8799294ac14a8ed08
Katrin Tomanek <katrin.tomanek@gmx.de>
2018-12-01 13:01:55 -0800

    Update gender.js

--- gender/gender.js
+++ gender/gender.js
@@ -1,3 +1,8 @@
-import * as tf from '@tensorflow/tfjs';
-const model = await tf.loadModel('web_model_gender/model.json');
+//import * as tf from '@tensorflow/tfjs';
+async function loadModel(){
+  model = await tf.loadModel('web_model_gender/model.json')
+}
+
+loadModel()\
+//const model = await tf.loadModel('web_model_gender/model.json');
 //<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.12.0"> </script>
