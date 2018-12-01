bf1b17a9b98d8c4b0b9d544b7c46929ba75a1d7c
Katrin Tomanek <katrin.tomanek@gmx.de>
2018-12-01 13:16:53 -0800

    Update gender.js

--- gender/gender.js
+++ gender/gender.js
@@ -1,7 +1,14 @@
 async function loadModel(){
-  model = await tf.loadModel('web_model_gender/model.json')
+  model = await tf.loadModel('web_model_mnist_keras/model.json')
+  //web_model_gender/model.json
 }
 
 loadModel()
 
 const tensor = tf.scalar(2);
+
+
+const model2 = tf.sequential();
+model2.add(tf.layers.dense({inputShape: [4], units: 100}));
+model2.add(tf.layers.dense({units: 4}));
+model2.compile({loss: ‘categoricalCrossentropy’, optimizer: ‘sgd’});
