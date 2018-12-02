7f18e0911c95dd5426cb55f23e9885977ddd4a2c
Katrin Tomanek <katrin.tomanek@gmx.de>
2018-12-01 16:16:06 -0800

    Update gender.js

--- gender/gender.js
+++ gender/gender.js
@@ -1,20 +1,26 @@
-protected async loadModel() {
-    this.model = await tf.loadModel('web_model_mnist_keras/model.json');
-}
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(100,100);
+ctx.stroke();
+
+// protected async loadModel() {
+//     this.model = await tf.loadModel('web_model_mnist_keras/model.json');
+// }
 
 
-protected async predict(imageData: ImageData) {
+// protected async predict(imageData: ImageData) {
 
-    const pred = await tf.tidy(() => {
+//     const pred = await tf.tidy(() => {
 
-      let img:any = tf.fromPixels(imageData, 1);
-      img = img.reshape([1, 28, 28, 1]);
-      img = tf.cast(img, 'float32');
+//       let img:any = tf.fromPixels(imageData, 1);
+//       img = img.reshape([1, 28, 28, 1]);
+//       img = tf.cast(img, 'float32');
 
-      const output = this.model.predict(img) as any;
+//       const output = this.model.predict(img) as any;
 
-      this.predictions = Array.from(output.dataSync());
-    });
-}
+//       this.predictions = Array.from(output.dataSync());
+//     });
+// }
 
-loadModel()
+// loadModel()
